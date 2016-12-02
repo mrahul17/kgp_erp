ba8085924a4573d15548496f0ff2db6db3c7a27c
Rahul Mishra <mishra.rahul1712@gmail.com>
2016-12-02 23:37:16 +0530

    Fetch resume from erp

---
+++ resume_fetcher.py
@@ -0,0 +1,19 @@
+import requests
+from main import login, urls
+import csv
+import sys
+
+try:
+    s = login()
+except:
+    sys.exit("Unable to login. Exiting..")
+
+with open('roll_list.csv') as f:
+    csvfile = csv.reader(f)
+    for row in csvfile:
+        r = s.get(urls['CV_URL'].format(row[0]))
+        if r.content != '':
+            with open("{0}.pdf".format(row[0]),'w+') as pdf:
+                pdf.write(r.content)
+
+s.close()
